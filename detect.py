1eacd60f76f3170a2fed4742e85fd1fd3bf9f93b
Nikita Karetnikov <NikitaKaretnikov@gmail.com>
2023-12-10 23:17:54 +0300

    add slice_video

--- detect.py
+++ detect.py
@@ -1,14 +1,16 @@
 from ultralytics import YOLO
-
-
-# Load a model
-model = YOLO('yolov8n.pt')  # pretrained YOLOv8n model
+import os
 
 # model = YOLO("yolov8n.yaml")  # build a new model from scratch
-# Use the model
 # model.train(data="coco128.yaml", epochs=3)  # train the model
 # metrics = model.val()  # evaluate model performance on the validation set
-# results = model("https://ultralytics.com/images/bus.jpg")  # predict on an image
 # Run batched inference on a list of images
 # result = model('bus.jpg')  # return a list of Results objects
-model.predict('street.jpg', save=True, imgsz=320, conf=0.5)
+
+fds = os.listdir("C:/Users/karet/PycharmProjects/object_detecting_yolo/src/example2")
+model = YOLO('yolov8n.pt')  # pretrained YOLOv8n model
+count = 0
+for img in fds:
+    model.predict('src/example2/' + img, save=True, imgsz=320, conf=0.5)
+    count += 1
+    print(f'Изображение {count} обработано')
